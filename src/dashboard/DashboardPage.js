362baac3daf4de88b732a8ff8612c7f0f34227ea
Dmitrii Kilishek <mentalclear@gmail.com>
2022-01-26 09:23:27 -0500

    Continue 5.5 (5.6 already completed)

--- src/dashboard/DashboardPage.js
+++ src/dashboard/DashboardPage.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { useUserInfo } from '../user';
+import { useStockHistory } from '../stock-history';
+import { StockChart } from "./StockChart";
+
 
 export const DashboardPage = () => {
     const [userInfo, setUserInfo] = useUserInfo();
     const { cashValue, sharesValue, numberOfSharesOwned } = userInfo || {};
+    const stockHistory = useStockHistory();
+    const times = Object.keys(stockHistory);
+    const prices = Object.values(stockHistory);
 
     return (
         <>
             <h1 className="section-heading">Stock Trading App</h1>
             <div className="centered-container">
-                <h3>The stock chart will go here...</h3>
+                <StockChart 
+                    xValues={times}
+                    yValues={prices} 
+                />
                 <div className="financial-info-section">
                     <div className="info-item"> Current TSLA Share Price: $400</div>
                     <div className="info-item"> You currently own {numberOfSharesOwned} shares</div>
