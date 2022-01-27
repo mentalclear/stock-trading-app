baf36654abde2734e23590f836fee768fdea6e8e
Dmitrii Kilishek <mentalclear@gmail.com>
2022-01-27 09:21:38 -0500

    Continue with 5.7

--- src/dashboard/DashboardPage.js
+++ src/dashboard/DashboardPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useUserInfo } from '../user';
 import { useStockHistory } from '../stock-history';
 import { StockChart } from "./StockChart";
@@ -10,6 +10,27 @@ export const DashboardPage = () => {
     const stockHistory = useStockHistory();
     const times = Object.keys(stockHistory);
     const prices = Object.values(stockHistory);
+    const [numberOfShareValue, setNumberOfSharesValue] = useState(0);
+
+    const buyShares = async () => {
+        const response = await fetch('/stocks/buy', {
+            method: 'post',
+            body: JSON.stringify({numberOfShares: numberOfShareValue}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const updatedUserInfo = await response.json();
+        setUserInfo(updatedUserInfo);
+    }
+
+    const sellShares = async () => {
+        const response = await fetch('/stocks/sell', {
+            method: 'post',
+            body: JSON.stringify({numberOfShares: numberOfShareValue}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const updatedUserInfo = await response.json();
+        setUserInfo(updatedUserInfo);
+    }
 
     return (
         <>
@@ -25,6 +46,20 @@ export const DashboardPage = () => {
                     <div className="info-item"> Cash Balance: {cashValue}</div>
                     <div className="info-item"> Portfolio Value: ${sharesValue}</div>
                     <div className="info-item"> Total Value: ${cashValue + sharesValue}</div>
+                    <div>
+                        <input
+                            type="number"
+                            className="full-width space-after"
+                            value={numberOfShareValue}
+                            onChange={e=>setNumberOfSharesValue(e.target.value)}
+                        />
+                        <button 
+                            className="buy-button"
+                            onClick={buyShares}>Buy</button>
+                        <button 
+                            className="sell-button"
+                            onClick={sellShares}>Sell</button>
+                    </div>
                 </div>
             </div>
         </>       
